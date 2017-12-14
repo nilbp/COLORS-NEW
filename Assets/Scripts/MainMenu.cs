dbb270b516ab5758af3d5a780d0eee023cd299b8
Unknown <adamtubauvilar@enti.cat>
2017-12-14 10:28:24 +0100

    botons mainmenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void newGameBtn(string newlvl){
+	
+		SceneManager.LoadScene (newlvl);
+
+	}
+	public void ExitBtn(){
+
+		Application.Quit();
+
+	}
+	public void OptionsBtn(string options){
+
+		SceneManager.LoadScene (options);
+
+	}
+}

6d70cab0800b98afeb83fef9721c1f44a633f0af
Nil <nil.bellmunt@gmail.com>
2017-12-15 18:40:59 +0100

    Spawn Minions

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -10,6 +10,7 @@ public class spawner : MonoBehaviour {
 
 	public GameObject[] minionspawn;
 
+
 	public Transform nucli;
 
 	HexInfo SpawnHex;
@@ -18,15 +19,15 @@ public class spawner : MonoBehaviour {
 
 	int time2=0;
 	int time=0;
-
-	public float spawnTime = 5f;     
-
+	   
 	public float speed = 1;
 	private float startTime;
 	private float dist;
 
 	float distCover;
 	float fracjourn;
+
+	int MinionSpawnCounter=0;
 
 	void Start()
 	{
@@ -47,67 +48,107 @@ public class spawner : MonoBehaviour {
 		fracjourn = distCover / dist;
 
 	}
-
-
-	/*void spawnminion (){
-
-		minionspawn [0] = Instantiate (minionprefab [0], HexSpawn1.transform.position, Quaternion.Euler (0, 0, 0)) as GameObject;
-		minionspawn [1] = Instantiate (minionprefab [1], HexSpawn2.transform.position, Quaternion.Euler (0, 0, 0)) as GameObject;
-		//minionspawn [2] = Instantiate (minionprefab [2], spawnLocation [2].transform.position, Quaternion.Euler (0, 0, 0)) as GameObject;
-	}*/
-
-/*	void clicki(){
-
-
-
-		MinionsSpawejats[a] = Instantiate (minionspawn[a], SpawnHex2.transform.position, SpawnHex2.transform.rotation ) as GameObject;
-		IAMoveS (SpawnHex2 ,MinionsSpawejats, a);
-		a++;
-		}*/
-
-
+		
 	void Update(){
 
-		//clicki ();
+		SpawnManager ();
+
+	}
+	
+	void SpawnManager(){
+
+
 		time2++;
 		time ++;
 
-
-		IAMoveForward (SpawnHex);
-		IAMoveS (SpawnHex2);
-
-		//float step = speed * Time.deltaTime;
-		//transform.position = Vector3.MoveTowards (minionspawn[0].transform.position, nucli.position, step);
-
-		//Vector3 posicion = nucli.transform.position ;
-		//minionspawn[0].transform.position = new Vector3 (transform.position,nucli.position, step);
-		//minionspawn[0].transform.position = new Vector3(nucli.position.x, 0, nucli.position.z);
-
+		switch(MinionSpawnCounter){
+			case 0:
+				IAMoveForward (SpawnHex, minionspawn, 0);
+				break;
+			case 1:
+				IAMoveForward (SpawnHex, minionspawn, 1);
+				break;
+			case 2:
+				IAMoveForward (SpawnHex, minionspawn, 2);
+				break;
+			case 3:
+				IAMoveForward (SpawnHex, minionspawn, 3);
+				break;
+		}
+		Debug.Log (time);
 	}
 
+	void ResetSpawn(HexInfo SpawnHex, int XSpawnPos, int YSpawnPos){
+	
+		SpawnHex.x = XSpawnPos;
+		SpawnHex.y = YSpawnPos;
+	}
 
+	void IAMoveForward(HexInfo SpawnHex, GameObject[] Minion, int i){
 
-	void IAMoveForward(HexInfo SpawnHex){
-
-
-	if (SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].HexColor == Color.white) {
-
-			if (time > 50 && time < 100) {
-				minionspawn [0].transform.position = Vector3.Lerp (SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].transform.position, SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x + 1].transform.position, fracjourn); 
-				//yield return new WaitForSeconds (2f);
-				SpawnHex.x++;
-				time = 0;
-			}
-		} 
-
-	else if (SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].HexColor == Color.cyan) {
+		if (time > 50) {
 			
-			Destroy (minionspawn [0]);
-			SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].GetComponent<MeshRenderer> ().material.mainTexture = null;
-			SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].transform.localScale = new Vector3(1, 1, 1);
+			if (SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].HexColor == Color.white) {
 
+				Minion [i].transform.position = Vector3.Lerp (SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x].transform.position, SpawnHex.map.hexLines [SpawnHex.y].columns [SpawnHex.x + 1].transform.position, fracjourn); 
+				SpawnHex.x++;
+
+			
+			} else {
+
+				ColisionColorDetection (SpawnHex.map.hexLines[SpawnHex.y].columns [SpawnHex.x], Minion, i);
+			
+				ResetSpawn (SpawnHex, 0, 2);
+				MinionSpawnCounter++;
+				Destroy (Minion[i]);
+			}
+			time = 0;
 		}
+	}
 
+	void ColisionColorDetection(HexInfo SpawnHex, GameObject[] Minion, int i){
+	
+		MeshRenderer MinionColor = Minion[i].GetComponent<MeshRenderer> ();
+		MeshRenderer HexColor = SpawnHex.GetComponent<MeshRenderer> ();
+
+		switch (MinionColor.material.color) {
+
+		case Color.cyan:
+			
+			if (SpawnHex.HexColor == Color.cyan) {
+				
+				SpawnHex.ColorDensity++;
+
+				break;
+			}
+			else if (SpawnHex.HexColor == Color.magenta) {
+
+				SpawnHex.HexColor = Color.white;
+				SpawnHex.transform.localScale = new Vector3 (1, 1, 1);
+				HexColor.material.mainTexture = null;
+				break;
+			} 
+			else if (SpawnHex.HexColor == Color.yellow) {
+
+				SpawnHex.HexColor = Color.white;
+				SpawnHex.transform.localScale = new Vector3 (1, 1, 1);
+				HexColor.material.mainTexture = null;
+				break;
+
+			} else {
+
+				SpawnHex.HexColor = Color.white;
+				SpawnHex.transform.localScale = new Vector3 (1, 1, 1);
+				HexColor.material.mainTexture = null;
+				break;
+			}
+		
+		case Color.magenta:
+			break;
+		case Color.yellow:
+			break;
+		}
+	
 	}
 
 	void IAMoveS(HexInfo SpawnHex){
